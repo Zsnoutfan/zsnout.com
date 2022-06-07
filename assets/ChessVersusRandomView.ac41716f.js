ad3b737c9488feca144f65aa06d0dcc5e91e7676
Zsnoutfan <Zsnoutfan@users.noreply.github.com>
2022-06-07 16:28:00 +0000

    deploy: 19ae77469415eb38a05bcf14ca094d1c5eb30577

---
+++ assets/ChessVersusRandomView.ac41716f.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./ChessVersusAI.f689ea5e.js";import{d as n,o as s,e as m}from"./index.62914774.js";import"./LegalMoveChessboard.f27cd689.js";const i=n({__name:"ChessVersusRandomView",setup(a){function t(o){let e=o.moves({verbose:!0});return e[Math.floor(Math.random()*e.length)]}return(o,e)=>(s(),m(r,{move:t}))}});export{i as default};
